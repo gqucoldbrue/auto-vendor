21c892ff7471a08f6f752d3a3be04beda875223b
gqucoldbrue <gqucoldbrue@gmail.com>
2024-11-30 22:01:11 -0500

    Initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['SF Pro Display', 'system-ui', 'sans-serif'],
+        },
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+      },
+    },
+    plugins: [],
+  }
