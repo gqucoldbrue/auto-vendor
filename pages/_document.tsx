e979a5f7462ad38f0b05bdef62d0f68d95ef2ee0
gqucoldbrue <gqucoldbrue@gmail.com>
2024-12-18 12:29:56 -0500

    Updated contents of pages/_document.tsx file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,20 @@
-export {};
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {/* Add any custom head elements here */}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
